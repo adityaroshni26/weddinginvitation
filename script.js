bcbba26dc5637f3ed1344e7544a07cc8fadff047
adityaroshni26 <insomniac176@gmail.com>
2026-03-13 12:34:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function openInvite(){
+
+document.getElementById("intro").style.display="none";
+
+document.getElementById("site").style.display="block";
+
+document.getElementById("music").play();
+
+}
+
 const faders = document.querySelectorAll(".fade");
 
 const observer = new IntersectionObserver(entries => {
@@ -5,9 +15,7 @@ const observer = new IntersectionObserver(entries => {
 entries.forEach(entry => {
 
 if(entry.isIntersecting){
-
 entry.target.classList.add("visible");
-
 }
 
 });
@@ -16,22 +24,20 @@ entry.target.classList.add("visible");
 
 faders.forEach(el => observer.observe(el));
 
-
 const weddingDate = new Date("April 19, 2026 11:30:00").getTime();
 
-const timer = setInterval(function() {
+setInterval(function(){
 
 const now = new Date().getTime();
 
 const distance = weddingDate - now;
 
-const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const days = Math.floor(distance/(1000*60*60*24));
 
 document.getElementById("countdown").innerHTML =
 days + " days to go";
 
-}, 1000);
-
+},1000);
 
 const music = document.getElementById("music");
 
